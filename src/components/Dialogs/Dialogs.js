a1806768fc8e711ecb0c24e613b9f745fe4ad2c0
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-05-01 21:23:38 +0300

    23.5 lesson

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './Dialogs.module.scss'
+import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = props => {
     return (
-        <div>
-            123
+        <div className={styles.dialogs}>
+            <div className={styles.dialogItems}>
+                <DialogItem id='1' name='Andre'/>
+                <DialogItem id='2' name='Dimka'/>
+                <DialogItem id='3' name='Nikolay'/>
+                <DialogItem id='4' name='Arseniy'/>
+            </div>
+            <div className={styles.messageItems}>
+                <Message message='Привет как дела'/>
+                <Message message='У меня нормич'/>
+                <Message message='А у тебя?'/>
+                <Message message='все по феншую'/>
+            </div>
         </div>
     );
 };
---
+++ src/components/Dialogs/Message/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Message.module.scss'
+
+const Message = props => {
+    return (
+        <div>
+            {props.message}
+        </div>
+    );
+};
+
+export default Message;
