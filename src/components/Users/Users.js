8bfef34ba33f8e17f32f2460d1faec326f850df0
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-05-09 12:04:00 +0300

    lesson 54 componentDidMount

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,54 +3,58 @@ import styles from './users.module.scss'
 import Axios from "axios";
 import defaultAvatar from '../../assets/img/UT8o1ZTXytaXXagOFbXf.jpg'
 
-const Users = props => {
-    console.log('props.users: ', props.users);
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
 
-    const getUsers = () => {
-        if (props.users.length === 0) {
+    componentDidMount(): void {
+        if (this.props.users.length === 0) {
             Axios.get('https://social-network.samuraijs.com/api/1.0/users')
                 .then((response) => {
-                    console.log('response: ', response);
-                    props.setUsers(response.data.items)
+                    this.props.setUsers(response.data.items)
                 });
         }
-    };
+    }
 
-    return (
-        <div className={styles.root}>
-            {props.users.length === 0 ? <button onClick={getUsers}>get users</button> : null}
-            {props.users.map((user) => {
-                return (
-                    <div key={user.id} className={styles.wrapper}>
-                        <div className={styles.avatar}>
-                            <div><img
-                                src={user.photos.small !== null ? user.photos.small : defaultAvatar}
-                                alt='avatar'/>
+    render() {
+        return (
+            <div className={styles.root}>
+                {/*{this.props.users.length === 0 ? <button onClick={this.getUsers}>get users</button> : null}*/}
+                {this.props.users.map((user) => {
+                    return (
+                        <div key={user.id} className={styles.wrapper}>
+                            <div className={styles.avatar}>
+                                <div><img
+                                    src={user.photos.small !== null ? user.photos.small : defaultAvatar}
+                                    alt='avatar'/>
+                                </div>
+                                {user.followed ?
+                                    <button onClick={() => {
+                                        this.props.unfollow(user.id)
+                                    }}>Follow</button>
+                                    : <button onClick={() => {
+                                        this.props.follow(user.id)
+                                    }}>Unfollow</button>
+                                }
                             </div>
-                            {user.followed ?
-                                <button onClick={() => {
-                                    props.unfollow(user.id)
-                                }}>Follow</button>
-                                : <button onClick={() => {
-                                    props.follow(user.id)
-                                }}>Unfollow</button>
-                            }
+                            <div className={styles.descriptionUser}>
+                                <div className={styles.name}>
+                                    <div>{user.name}</div>
+                                    <div className={styles.status}>{user.status}</div>
+                                </div>
+                                <div className={styles.country}>
+                                    <div>'user.location.country',</div>
+                                    <div>'user.location.city'</div>
+                                </div>
+                            </div>
                         </div>
-                        <div className={styles.descriptionUser}>
-                            <div className={styles.name}>
-                                <div>{user.name}</div>
-                                <div className={styles.status}>{user.status}</div>
-                            </div>
-                            <div className={styles.country}>
-                                <div>'user.location.country',</div>
-                                <div>'user.location.city'</div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    );
-};
+                    )
+                })}
+            </div>
+        );
+    }
+}
 
 export default Users;
