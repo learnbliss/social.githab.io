5541cb162b34d3a03bac9918f4cd8be3e83a079a
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-05-09 20:28:31 +0300

    lesson 58 minimize mapDispatchToProps

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -18,24 +18,24 @@ class UsersContainer extends React.Component {
     }
 
     componentDidMount(): void {
-        this.props.toggleIsFetching(true);
+        this.props.toggleIsFetchingAC(true);
         if (this.props.users.length === 0) {
             Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
                 .then((response) => {
-                    this.props.toggleIsFetching(false);
-                    this.props.setUsers(response.data.items);
-                    this.props.setTotalCount(response.data.totalCount);
+                    this.props.toggleIsFetchingAC(false);
+                    this.props.setUsersAC(response.data.items);
+                    this.props.setTotalCountAC(response.data.totalCount);
                 });
         }
     }
 
     onPageChanged = (page) => {
-        this.props.toggleIsFetching(true);
-        this.props.setCurrentPage(page);
+        this.props.toggleIsFetchingAC(true);
+        this.props.setCurrentPageAC(page);
         Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
             .then((response) => {
-                this.props.toggleIsFetching(false);
-                this.props.setUsers(response.data.items)
+                this.props.toggleIsFetchingAC(false);
+                this.props.setUsersAC(response.data.items)
             });
     };
 
@@ -46,8 +46,8 @@ class UsersContainer extends React.Component {
                    currentPage={this.props.currentPage}
                    onPageChanged={this.onPageChanged}
                    users={this.props.users}
-                   unfollow={this.props.unfollow}
-                   follow={this.props.follow}
+                   unfollow={this.props.unfollowAC}
+                   follow={this.props.followAC}
                    isFetching={this.props.isFetching}/>
         );
     }
@@ -64,27 +64,36 @@ const mapStateToProps = (state) => {
     }
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followAC(userId))
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowAC(userId))
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users))
-        },
-        setCurrentPage: (currentPage) => {
-            dispatch(setCurrentPageAC(currentPage))
-        },
-        setTotalCount: (totalCount) => {
-            dispatch(setTotalCountAC(totalCount))
-        },
-        toggleIsFetching: (isFetching) => {
-            dispatch(toggleIsFetchingAC(isFetching))
-        }
-    }
-};
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         followAC: (userId) => {
+//             dispatch(followAC(userId))
+//         },
+//         unfollowAC: (userId) => {
+//             dispatch(unfollowAC(userId))
+//         },
+//         setUsersAC: (users) => {
+//             dispatch(setUsersAC(users))
+//         },
+//         setCurrentPageAC: (currentPage) => {
+//             dispatch(setCurrentPageAC(currentPage))
+//         },
+//         setTotalCountAC: (totalCount) => {
+//             dispatch(setTotalCountAC(totalCount))
+//         },
+//         toggleIsFetchingAC: (isFetching) => {
+//             dispatch(toggleIsFetchingAC(isFetching))
+//         }
+//     }
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+export default connect(mapStateToProps,
+    {
+        followAC,
+        unfollowAC,
+        setUsersAC,
+        setCurrentPageAC,
+        setTotalCountAC,
+        toggleIsFetchingAC,
+    }
+)(UsersContainer);
